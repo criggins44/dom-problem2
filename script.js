d3dcf358a77a624cc1bd0d513636f70925219638
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-15 15:40:26 -0400

    added comments to explain logic

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function myFunction(){
 	const link = document.getElementById("site").getAttribute("href");
+//creat a constant variable to hold the value of the href attribute of the link
+
 	document.getElementById("demo").innerHTML = link;
+//puts the attribute identified in the variable in the "demo" element
+//new element is displayed when buttun is clicked
 }
